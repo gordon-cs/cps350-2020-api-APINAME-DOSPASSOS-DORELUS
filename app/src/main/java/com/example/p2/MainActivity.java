e824c6822604066e86a16ae3d8beed1285a4b328
jahnueldorelus <jahnueldorelus@gmail.com>
2020-04-10 17:47:00 -0400

    Updated comments in MainActivity.java

--- app/src/main/java/com/example/p2/MainActivity.java
+++ app/src/main/java/com/example/p2/MainActivity.java
@@ -24,38 +24,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //binds myImage variable to the ImageView with the Id "MyImage"
+        // Binds myImage variable to the ImageView with the Id "MyImage"
         myImage = (ImageView)findViewById(R.id.MyImage);
 
-        //binds button variable to the Button with the Id "Button"
+        // Binds button variable to the Button with the Id "Button"
         button = (Button)findViewById(R.id.Button);
 
-        //creates a on click listener that calls our take picture function when ever our button is clicked
+        // Creates a on click listener that calls our take picture function when ever our button is clicked
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                // Checks to see if permission is granted to the application to use the camera. If not
-                // the user is prompted to allow permission
+                /* Checks to see if permission is granted to the application to use the camera. If not
+                 * the user is prompted to allow permission
+                 */
                 if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
                         == PackageManager.PERMISSION_GRANTED) {
-                    // Permission granted
+                    // Permission granted - Calls function to take a picture
                     takePicture();
                 } else {
-                    // Permission not granted
+                    /* Permission not granted - Ask user for permission again. If they deny it
+                     * indefinitely, nothing will appear
+                     */
                     ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.CAMERA}, 1);
                 }
             }
         });
     }
-    //function creates a intent(API call) that opens the camera and resolves when we take a picture
+
+    // Function creates a intent(API call) that opens the camera and resolves when we take a picture
     private void takePicture(){
         Intent picture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(picture, 0);
 
     }
 
-    // when the intent is resolved it takes the data from the intent(in this case, the picture)
-    //and sets it to our image view
+    /* When the intent is resolved it takes the data from the intent(in this case, the picture)
+     * and sets it to our image view
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
